425fd26ac12aeefc1bc17d5867148af9c52ab19b
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-03-15 17:06:39 -0400

    word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,17 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i]).join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
     }
+    for (let l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
